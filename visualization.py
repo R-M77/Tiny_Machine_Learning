676aba8301753df732771ace50ad9834ee462dcf
R-M77 <artstarss@gmail.com>
2021-12-01 11:00:57 -0500

    add data shape specification

--- visualization.py
+++ visualization.py
@@ -4,13 +4,14 @@ import tensorflow as tf
 
 
 """visualize data"""
-def show_images(img,val_images):
+def show_images(img,validation_data,data_shape):
     plt.figure()
-    plt.imshow(val_images[img].reshape(28,28))
+    plt.imshow(val_images[img].reshape(data_shape[0],data_shape[1],data_shape[2]))
     plt.grid(False)
     plt.axis('off')
     plt.show()
 
+# set up plots
 f, axarr = plt.subplots(3,2)
 first_image = 2
 second_image = 3
@@ -21,19 +22,23 @@ from tensorflow.keras import models
 layer_outputs = [layer.output for layer in model.layers]
 activation_model = tf.keras.models.Model(inputs=model.input, outputs = layer_outputs)
 
+# create color map for each output for visualization
+data_shape = (28,28,1)      #specify data shape (x_dim, y_dim, channels)
+
 for x in range(0,2):
-    f1 = activation_model.predict(val_images[first_image].reshape(1,28,28,1))[x]
+    f1 = activation_model.predict(val_images[first_image].reshape(1,data_shape[0],data_shape[1],data_shape[2]))[x]
     axarr[0,x].imshow(f1[0,:,:,convolution_number],cmap = 'inferno')
     axarr[0,x].grid(False)
 
-    f2 = activation_model.predict(val_images[second_image].reshape(1,28,28,1))[x]
+    f2 = activation_model.predict(val_images[second_image].reshape(1,data_shape[0],data_shape[1],data_shape[2]))[x]
     axarr[1,x].imshow(f2[0,:,:,convolution_number],cmap = 'inferno')
     axarr[1,x].grid(False)
 
-    f3 = activation_model.predict(val_images[third_image].reshape(1,28,28,1))[x]
+    f3 = activation_model.predict(val_images[third_image].reshape(1,data_shape[0],data_shape[1],data_shape[2]))[x]
     axarr[2,x].imshow(f3[0,:,:,convolution_number],cmap = 'inferno')
     axarr[2,x].grid(False)
 
-show_images(first_image,val_images)
-show_images(second_image,val_images)
-show_images(third_image,val_images)
+# display images altogether
+show_images(first_image,validation_data=val_images,data_shape=data_shape)
+show_images(second_image,validation_data=val_images,data_shape=data_shape)
+show_images(third_image,validation_data=val_images,data_shape=data_shape)
